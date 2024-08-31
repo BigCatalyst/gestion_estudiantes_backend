bc40317765ad0ed1b0de5fe159ee73be9677c406
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2024-08-31 01:17:25 -0400

    reporte altas bajas

--- src/cu/edu/unah/demo/controller/GraduadoController.java
+++ src/cu/edu/unah/demo/controller/GraduadoController.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import cu.edu.unah.demo.model.*;
+import cu.edu.unah.demo.reportes.ReportesUtiles;
 import cu.edu.unah.demo.services.*;
 import java.util.HashMap;
 import javax.persistence.EntityExistsException;
+import org.hibernate.id.IdentifierGenerationException;
 
 @RequestMapping("/Graduado")
 @RestController
@@ -97,6 +99,22 @@ public class GraduadoController {
             HashMap<String, String> response = new HashMap<>();
             response.put("error", e.getMessage());
             return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+    
+    @GetMapping(path = {"/reporte"}, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity reporte() {
+        try {
+            return ReportesUtiles.generarReporte(graduadoservices.getDatosGraduado());
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (EntityExistsException | BadRequestException | IdentifierGenerationException e) {
+            HashMap<String, String> response = new HashMap<>();
+            response.put("error", e.getMessage());
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/cu/edu/unah/demo/services/GraduadoServices.java
+++ src/cu/edu/unah/demo/services/GraduadoServices.java
@@ -32,4 +32,26 @@ public class GraduadoServices {
 	public void delete(Integer id) {
 		graduadorepository.deleteById(id);
 	}
+        public String[][] getDatosGraduado() {
+            return getDatosGraduado(findAll());
+        }
+        private String[][] getDatosGraduado(List<Graduado> graduados) {
+        String[] titulos = new String[]{"Ci", "Nombre", "Apellido", "Carrera", "No.Es","Genero","Nota"};
+        String[][] datos = new String[graduados.size() + 2][titulos.length];
+        datos[0] = titulos;
+        datos[1] = new String[]{"2", "2", "2", "2", "1","1","1"};
+        int row = 2;
+        for (Graduado estudiante : graduados) {
+            datos[row][0] = estudiante.getCi();
+            datos[row][1] = estudiante.getNombre();
+            datos[row][2] = estudiante.getApellidos();
+            datos[row][3] = estudiante.getCarrera()+"";
+            datos[row][4] = estudiante.getNoescalafon()+"";
+            datos[row][5] = estudiante.getSexo()+"";
+            datos[row][6] = estudiante.getNotaescalafon()+"";
+            
+            row++;
+        }
+        return datos;
+    }
 }
